aa41d5f2f6018856e3f59e3456c8768ce032fc8a
JimW1 <gccc202020@gmail.com>
2022-01-24 01:31:58 +1000

    comment cleanup

--- nomjpg.cc
+++ nomjpg.cc
@@ -20,20 +20,14 @@ exit 0
 #include <iostream>
 #include <sstream>
 
-
 using namespace cimg_library;
-
-
-#ifndef cimg_imagepath
-#define cimg_imagepath "img/"
-#endif
 
 #define nomjpg  main
 #define IN      int
 #define CH      char
 #define CCH     const CH
 #define CCS     CCH *
-#define $$$     IN argc, CH **argv
+//#define $$$     IN argc, CH **argv
 
 #define LOG                  std::cout
 #define STRING               std::string
@@ -50,21 +44,16 @@ using namespace cimg_library;
 STRING IN2STR(IN x) { std::stringstream ss; ss << x; return ss.str(); }
 
 //IN main($$$) {
-IN nomjpg($$$) {
+IN nomjpg() {
   IN cwinwidth = CWINDOWWIDTH;
   IN cwinheight = CWINDOWHEIGHT;
-  cimg_usage("Trim an image with wasd/jk, confirm with c (crop/cycle)"); // -h
+//  cimg_usage("crop images - wasd,tfgh,ijkl,c,arrows"); // -h
   STRING imgfolder = "./img";
   STRING omgfolder = "./omg";
   FSPATH imglist;
   std::copy(DIRITER(imgfolder), DIRITER(), std::back_inserter(imglist));
   std::sort(imglist.begin(), imglist.end());
-
-//for (const std::string & filename : files_in_directory) {
-//    std::cout << path.string() << std::endl; // printed in alphabetical order
-//}
   CImgDisplay imagewindow(cwinwidth, cwinheight, "NO IMAGE", 0);
-//  IN cropmode = 1; // crop until uncrop
   IN numfiles = (IN)imglist.size();
   IN fileix = 0; // align to first image
   IN prevfileix = -1; // initialise first image
@@ -93,11 +82,6 @@ IN nomjpg($$$) {
       return 0; // exit immediately
     } else if (imagewindow.is_keyQ()) {
       return 0; // exit immediately
-//    } else if (imagewindow.is_keyA()) {
-//      if (omgimagewidth > 1) { cropleft++;  updatetitle = 1; }
-//      if (imagewindow.is_keySHIFTLEFT()) { // shift+nom=move
-//        if (cropright > 0)   { cropright--; updatetitle = 1; }
-//      }
     // grow
     } else if (imagewindow.is_keyA()) {
       if (omgimagewidth > 1) { cropleft++;  updatetitle = 1; }
@@ -144,7 +128,6 @@ IN nomjpg($$$) {
       // else endoflist animation
     // if no keys are pressed, update the image
     } else if (updatetitle == 1) { updatetitle = 2; } // key release
-
     if (updatetitle > 0) { // need to update on initial init too
       if (prevfileix != fileix) {
         imgpath = imglist.at(fileix);
@@ -154,8 +137,6 @@ IN nomjpg($$$) {
         } else { omgpath = ""; } // don't open the empty one !
         // update paths if index changed (on keypress as well as keyup)
       } // don't update prevfileix until image changes
-std::cout << "Test3... imgpath: " << imgpath << ", omgpath: " << omgpath << ", updatetitle: " << updatetitle << "\n";
-
       if (updatetitle > 1) { // key released (updatetitle == 2)
         titleupdates = 0; // reset periodic keyup counter
         updatetitle = 0; // 0 until next keydown
@@ -169,8 +150,6 @@ std::cout << "Test3... imgpath: " << imgpath << ", omgpath: " << omgpath << ", u
           // todo: load a default error image?
           imagewidth = image.width();
           imageheight = image.height();
-std::cout << "Test4... imagewidth: " << imagewidth << ", imageheight: " << imageheight << "\n";
-
           // load outcache crop data if present
           cropleft = cropright = cropup = cropdown = 0; // reset before load
           if (omgpath.length() > 0) { // if outcache/outfile path is set
@@ -214,11 +193,6 @@ std::cout << "Test4... imagewidth: " << imagewidth << ", imageheight: " << image
                 }
                 // if omgchar is not EOF something went wrong
                 fclose(omgfile);
-//            omgtitle += " [" + IN2STR(cropleft);
-//            omgtitle += ", " + IN2STR(cropright);
-//            omgtitle += ", " + IN2STR(cropup);
-//            omgtitle += ", " + IN2STR(cropdown);
-//            omgtitle += "] -> " + omgfolder;
               } // end of reading valid file that started with [
             } // else no omgfile to load details from
           } // crop details have been loaded. update the image
@@ -284,78 +258,4 @@ std::cout << "Test4... imagewidth: " << imagewidth << ", imageheight: " << image
   return 1;
 }
 
-/*
-
-//  for (const std::string & filename : imglist) {
-//    std::cout << path.string() << std::endl; // printed in alphabetical order
-//      std::cout << filename << std::endl; // printed in alphabetical order
-//  }
-//  if (1) { return 0; }
-  // Read image filename from the command line (or set it to "img/parrot.ppm" if option '-i' is not provided).
-//  CCS file_i = cimg_option("-i",cimg_imagepath "parrot.ppm","Input image");
-  // Read pre-blurring variance from the command line (or set it to 1.0 if option '-blur' is not provided).
-//  const double sigma = cimg_option("-blur",1.0,"Variance of gaussian pre-blurring");
-  // Load an image, transform it to a color image (if necessary) and blur it with the standard deviation sigma.
-
-  // Create two display window, one for the image, the other for the color profile.
-  CImgDisplay
-    main_disp(image,"Color image (Try to move mouse pointer over)",0),
-    draw_disp(500,400,"Color profile of the X-axis",0);
-
-  // Define colors used to plot the profile, and a hatch to draw the vertical line
-  unsigned int hatch = 0xF0F0F0F0;
-  const unsigned char
-    red[]   = { 255,0,0 },
-    green[] = { 0,255,0 },
-    blue [] = { 0,0,255 },
-    black[] = { 0,0,0 };
-
-    // Enter event loop. This loop ends when one of the two display window is closed or
-    // when the keys 'ESC' or 'Q' are pressed.
-    while (!main_disp.is_closed() && !draw_disp.is_closed() &&
-           !main_disp.is_keyESC() && !draw_disp.is_keyESC() && !main_disp.is_keyQ() && !draw_disp.is_keyQ()) {
-
-      // Handle display window resizing (if any)
-      if (main_disp.is_resized()) main_disp.resize().display(image);
-      draw_disp.resize();
-
-      if (main_disp.mouse_x()>=0 && main_disp.mouse_y()>=0) { // Mouse pointer is over the image
-
-        const int
-          xm = main_disp.mouse_x(),                     // X-coordinate of the mouse pointer over the image
-          ym = main_disp.mouse_y(),                     // Y-coordinate of the mouse pointer over the image
-          xl = xm*draw_disp.width()/main_disp.width(),  // Corresponding X-coordinate of the hatched line
-          x = xm*image.width()/main_disp.width(),     // Corresponding X-coordinate of the pointed pixel in the image
-          y = ym*image.height()/main_disp.height();   // Corresponding Y-coordinate of the pointex pixel in the image
-
-        // Retrieve color component values at pixel (x,y)
-        const unsigned int
-          val_red   = image(x,y,0),
-          val_green = image(x,y,1),
-          val_blue  = image(x,y,2);
-
-        // Create and display the image of the intensity profile
-        CImg<unsigned char>(draw_disp.width(),draw_disp.height(),1,3,255).
-          draw_grid(-50*100.0f/image.width(),-50*100.0f/256,0,0,false,true,black,0.2f,0xCCCCCCCC,0xCCCCCCCC).
-          draw_axes(0,image.width() - 1.0f,255.0f,0.0f,black).
-          draw_graph(image.get_shared_row(y,0,0),red,1,1,0,255,1).
-          draw_graph(image.get_shared_row(y,0,1),green,1,1,0,255,1).
-          draw_graph(image.get_shared_row(y,0,2),blue,1,1,0,255,1).
-          draw_text(30,5,"Pixel (%d,%d)={%d %d %d}",black,0,1,16,
-                    main_disp.mouse_x(),main_disp.mouse_y(),val_red,val_green,val_blue).
-          draw_line(xl,0,xl,draw_disp.height() - 1,black,0.5f,hatch=cimg::rol(hatch)).
-          display(draw_disp);
-      } else
-        // else display a text in the profile display window.
-        CImg<unsigned char>(draw_disp.width(),draw_disp.height()).fill(255).
-          draw_text(draw_disp.width()/2 - 130,draw_disp.height()/2 - 5,"Mouse pointer is outside the image",
-                    black,0,1,16).display(draw_disp);
-
-      // Temporize event loop
-      cimg::wait(20);
-    }
-
-    return 0;
-}
-*/
 #endif
